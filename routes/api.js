2b9f84157eef6f85496c25f720d000eda4a74c3a
Thomas Beirne <thomas.beirne@logos.com>
2013-12-27 20:23:50 -0800

    Handle errors thrown during data access.

--- routes/api.js
+++ routes/api.js
@@ -8,9 +8,22 @@ var r = require('rethinkdb'),
 
 function createCrud(entityName) {
     var pascalEntityName = entityName[0].toUpperCase() + entityName.substring(1);
+    function handleErrors(fn, res) {
+        return function () {
+            try {
+                fn.apply(this, arguments);
+            }
+            catch (e) {
+                if (e.name === 'RqlDriverError')
+                    res.status(500).json({ error: 'RqlDriverException.' });
+                else
+                    res.status(500).json({ error: 'Unknown' });
+            }
+        }
+    }
     
     exports['get' + pascalEntityName + 's'] = function (req, res) {
-        r.connect(connConfig, function (err, conn) {
+        r.connect(connConfig, handleErrors(function (err, conn) {
             if (err) throw err;
 
             r.db('rpgmaker').table(entityName + 's').run(conn, function (err, cur) {
@@ -21,25 +34,25 @@ function createCrud(entityName) {
                     res.json(jsonResult);
                 });
             });
-        }); 
+        }, res));
     };
 
     exports['get' + pascalEntityName] = function (req, res) {
-        r.connect(connConfig, function (err, conn) {
+        r.connect(connConfig, handleErrors(function (err, conn) {
             r.db('rpgmaker').table(entityName + 's').get(req.params[entityName + 'Id']).run(conn, function (err, entity) {
                 res.json(entity);
             });
-        });
+        }, res));
     };
 
     exports['create' + pascalEntityName] = function (req, res) {
-        r.connect(connConfig, function (err, conn) {
+        r.connect(connConfig, handleErrors(function (err, conn) {
             r.db('rpgmaker').table(entityName + 's').insert(req.body).run(conn, function (err, result) {
                 if (err)
                     throw new Error(err);
                 res.json({ id: result.generated_keys[0] }, 201);
             });
-        });
+        }, res));
     };
 
     exports['update' + pascalEntityName] = function (req, res) {
@@ -47,7 +60,7 @@ function createCrud(entityName) {
 
     exports['delete' + pascalEntityName] = function (req, res) {
         console.log('DELETE');
-        r.connect(connConfig, function (err, conn) {
+        r.connect(connConfig, handleErrors(function (err, conn) {
             console.log('CONNECTED');
             r.db('rpgmaker').table(entityName + 's').get(req.params[entityName + 'Id']).delete().run(conn, function (err, result) {
                 console.log('RUNNING');
@@ -58,7 +71,7 @@ function createCrud(entityName) {
                 else
                     res.send(204);
             });
-        });
+        }, res));
     };
 }
 
